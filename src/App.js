8e8bc2500e8a1fb1b4b55c2a239da188330b4753
AlexJmzc <73499509+AlexJmzc@users.noreply.github.com>
2023-01-27 15:36:25 -0500

    Header y CSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-
+import React from 'react';
 import './App.css';
+import Header from './componentes/header/header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-        <p>
-          Edit <code>src/App.js</code> and  to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Header/>
+    </>
+  )
 }
 
 export default App;
