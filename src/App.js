7fc0372d31b24537f6c470d212678f244846683e
AlexJmzc <73499509+AlexJmzc@users.noreply.github.com>
2023-01-27 17:57:49 -0500

    Pantalla principal e información general

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from './componentes/header/header';
+import Home from './componentes/home/Home';
 
 function App() {
   return (
     <>
     <Header/>
+
+    <main className='main'>
+        <Home />
+    </main>
     </>
   )
 }
---
+++ src/componentes/home/Data.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+const Data = () => {
+    return (
+        <div className="home__data">
+            <h1 className="home__title">
+                Javier Jiménez
+            </h1>
+
+            <h3 className="home__subtitle">
+                Desarrollador de Software
+            </h3>
+
+            <p className="home__description">
+                Soy un desarrollador de Software de Ambato, dedicado y responsable con mi trabajo,
+                y con muchas ganas de seguir ampliando mis conocimientos.
+            </p>
+
+            <a href="#contact" className="button button--flex">
+                Di Hola
+            </a>
+        </div>
+    )
+}
+
+export default Data
